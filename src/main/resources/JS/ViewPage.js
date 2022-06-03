99fb7760e464095fddbefad846ef84df2d768ac5
Soham Choudhury <soham874@gmail.com>
2022-06-04 00:29:45 +0530

    Added : Navbar facility
    Drop down and go up facility for navbar

--- src/main/resources/JS/ViewPage.js
+++ src/main/resources/JS/ViewPage.js
@@ -1,4 +1,36 @@
+let isclicked = false
+
 $(document).ready(function() {
-    //alert("Javascript file connected successfully");
+
     console.log("Javascript file connected successfully");
+
+    $(".header-links").on(
+        'click',
+        function(event) {
+
+            if (window.innerWidth > 1250)
+                return
+
+            var elems = document.getElementsByClassName("header-links")
+            for (var i = 0; i < elems.length; i += 1)
+                elems[i].style.display = 'none';
+
+        }
+    )
 });
+
+showmenu = () => {
+    console.log("menu show");
+
+    if (!isclicked) {
+        var elems = document.getElementsByClassName("header-links")
+        for (var i = 0; i < elems.length; i += 1)
+            elems[i].style.display = 'block';
+
+    } else {
+        var elems = document.getElementsByClassName("header-links")
+        for (var i = 0; i < elems.length; i += 1)
+            elems[i].style.display = 'none';
+    }
+    isclicked = !isclicked;
+}
