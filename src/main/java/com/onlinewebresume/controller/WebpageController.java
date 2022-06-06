e7c9c7ee837403620d578ea19eb9af1151cde077
Soham Choudhury <soham874@gmail.com>
2022-06-06 11:12:49 +0530

    Tested : First major release
    Deployed Webapp to Heroku. Frontend working as expected

---
+++ src/main/java/com/onlinewebresume/controller/WebpageController.java
@@ -0,0 +1,13 @@
+package com.onlinewebresume.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebpageController {
+	// Displaying the initial users list.
+    @RequestMapping(value = "/")
+    public String welcome() {     
+        return "ViewPage";
+    }
+}
