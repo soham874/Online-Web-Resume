55288679c083620aa7d7b074dcb9ccca98406ce9
Soham Choudhury <soham874@gmail.com>
2022-06-21 15:57:53 +0530

    Tested : Google Sheets API

--- src/main/java/com/services/TestService.java
+++ src/main/java/com/services/TestService.java
@@ -13,16 +13,11 @@ public class TestService {
 	private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 	public static void main(String[] args) throws IOException {
 		
-		String request_json = ""
-				+ "{\r\n"
-				+ "  \"dataSource\": \"SohamsWebPortal\",\r\n"
-				+ "  \"database\": \"Web_Portal_Database\",\r\n"
-				+ "  \"collection\": \"User_Responses\",\r\n"
-				+ "  \"document\": {\r\n"
-				+ "    \"Name\": \"Soham\",\r\n"
-				+ "    \"TimeStamp\": \"Sat Jun 18 2022 12:08:53 GMT+0530 (India Standard Time)\",\r\n"
-				+ "    \"Message\": \"This is a test response\"\r\n"
-				+ "  }\r\n"
+		String request_json = "{\r\n"
+				+ "    \"timestamp\": \"time2\",\r\n"
+				+ "    \"browser\": \"text\",\r\n"
+				+ "    \"height\": \"sample\",\r\n"
+				+ "    \"width\": \"sample\"\r\n"
 				+ "}";
 		
 		OkHttpClient client = new OkHttpClient().newBuilder().build();
@@ -31,11 +26,7 @@ public class TestService {
 		RequestBody body = RequestBody.create(request_json,JSON);
         Request request = new Request.Builder()
         		/*.url("https://hookb.in/JKdXbjy6O0sg1WBgVnxa")*/
-            .url("https://data.mongodb-api.com/app/data-okjli/endpoint/data/v1/action/insertOne")
-            .header("content-type", "application/ejson")
-            .addHeader("Accept", "application/json")
-            .addHeader("api-key", Webapp_key_params.getMongoDB_Api_Key() )
-            .addHeader("Access-Control-Request-Headers", "*")
+            .url("https://sheetdb.io/api/v1/sa6miu6hdqxos")
             .post(body)
             .build();
         
