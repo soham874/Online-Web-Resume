0e717239acbde6f5114e712ba97ac304ba18ca9f
Soham Choudhury <soham874@gmail.com>
2022-10-21 00:30:51 +0530

    Changed cron job to run every 10 minutes

--- src/main/java/com/SohamsOnlineWebPortal/SohamsOnlineWebPortalApplication.java
+++ src/main/java/com/SohamsOnlineWebPortal/SohamsOnlineWebPortalApplication.java
@@ -3,8 +3,10 @@ package com.SohamsOnlineWebPortal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @EnableScheduling
+//@PropertySource("file://https://drive.google.com/file/d/10OsvSq7gXwbJGD6L81qTMZqFvTsiPI17/view?usp=sharing")
 public class SohamsOnlineWebPortalApplication {
 
 	public static void main(String[] args) {
@@ -24,14 +27,18 @@ public class SohamsOnlineWebPortalApplication {
 @RestController 
 class HomeContoller {
 	
+	//@Value("${spring.property.testvalue}")
+	//private String check;
+	
     @RequestMapping("/")
     public ModelAndView index() {
     	ModelAndView modelAndView = new ModelAndView();
+    	System.out.println("Application connected successfully -> "+check);
         modelAndView.setViewName("ViewPage.html");
         return modelAndView;
     }
 	
-	@Scheduled(fixedDelay = 5000)
+	@Scheduled(fixedDelay = 600000)
 	public void scheduleFixedDelayTask() {
 	    System.out.println(
 	      "Cron job to keep application awake -> " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()) );
