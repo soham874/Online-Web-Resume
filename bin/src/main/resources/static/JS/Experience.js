0bf1a6aaa4b5f4236d288a4b5c1b93fdce29269f
Soham Choudhury <soham874@gmail.com>
2022-10-31 00:38:39 +0530

    fix unintented rm operation

---
+++ bin/src/main/resources/static/JS/Experience.js
@@ -0,0 +1,205 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ To be fetched from Database ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var isExpanded = []
+
+var experience_fallback = [{
+    "_id": "62ac965af4d9e59abac0f331",
+    "Role": "Software Engineer",
+    "Name": "hcl",
+    "Logo_Url": "https://drive.google.com/file/d/1kyKrk10j-Yd4Cxyk4aNBtMVt7jJj-n9I/view?usp=sharing",
+    "Organization": "HCL Technologies Limited",
+    "Location": "Nagpur, India",
+    "Department": "Project Lifecycle Management, Engineering R&D Services",
+    "Project": "Digital Work Place",
+    "Client": "A leading European civilian and military Aircraft manufacturer",
+    "Description": "Project aim is establishing a modernised digital workplace to enhance the user experience and service quality for the majority of client employees in the EU region. Project activities include deployment and maintenance of crucial CAD applications on client user machines, non-user worker machines and blade servers, and handling all related incidents and service requests.",
+    "Responsibilities": [
+        "Developing tools via scripting in Powershell, Bash, CMD and C# to facilitate and automate project activities",
+        "Coordinating with business to plan timelines and deploy new versions of CATIA on VMs, Laptops, Desktops and Servers via SCCM",
+        "Carrying out pre-deployment activities, testing and sanity checks to ensure health of delivered package.",
+        "Regularly monitoring target machines parameters for anomalies to ensure maximum success of deployment.",
+        "Participating in post deployment monitoring activities to provide resolutions to possible incidents",
+        "Using SQL and WQL to ensure overall health of all EU PLM park machines, and taking action wherever necessary.",
+        "Coordinating with teams and end users to resolve incidents and complete service requests as and when raised by client users"
+    ],
+    "IDE/Compilers/Tools": [
+        "Microsoft Configuration Management Console to manage and monitor deployments in several formats",
+        "ServiceNow to handle incident and requests from client",
+        "SQL and WQL for performing activities on SCCM Database with an information pool of more than 100,000 machines"
+    ],
+    "Duration": "8th Feb 2021 - Present",
+    "Serial_Number": 1
+},{
+    "Role": "Software Development Engineer 2",
+    "Name": "walmart",
+    "Logo_Url": "https://drive.google.com/file/d/110r2lMZL8DuzYA8_LpOGqLXLQaTHLCXq/view?usp=sharing",
+    "Organization": "Walmart Global Technology Services India",
+    "Location": "Bengaluru, Karnataka, India",
+    "Department": "Sams Club",
+    "Project": "STEEL Development",
+    "Client": "Walmart",
+    "Description": "",
+    "Responsibilities": [
+        
+    ],
+    "IDE/Compilers/Tools": [
+        
+    ],
+    "Duration": "11th July 2022 - Present",
+    "Serial_Number": 2
+}]
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ To be fetched from Database ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// functions to be performed when webpage loads
+$(document).ready(function() {
+
+    setTimeout( () => {
+        $.ajax({
+            type: 'GET',
+            url: formAjaxUrl("receiveExpereienceData"),
+            headers: {
+                'session-uid':makeid()
+            },
+            contentType: "application/json",
+            dataType: 'json',
+            success: (data_expereince_mongo) => {
+                if (data_expereince_mongo.status >= 200 && data_expereince_mongo.status < 400) {
+                    console.log("AJAX RESPONSE >> Success Expereicne, loading live data")
+                    document.getElementById("work_error").style.display = 'none'
+                    document.getElementById("work-exp").style.display = 'block'
+                        //console.log(data_expereince_mongo.body)
+                    loadExperience(data_expereince_mongo.body.documents)
+                } else {
+                    console.log("AJAX RESPONSE >> Error Expereicne, loading fallback data")
+                        //document.getElementById("work-exp").style.display = 'none'
+                        //document.getElementById("work_error").style.display = 'flex'
+                    loadExperience(experience_fallback)
+                }
+            },
+            error: (err) => {
+                console.log("AJAX RESPONSE >> Error Expereicne, loading fallback data")
+                    //document.getElementById("work-exp").style.display = 'none'
+                    //document.getElementById("work_error").style.display = 'flex'
+                loadExperience(experience_fallback)
+            }
+        })
+
+    },1000)
+
+    console.log("Experience JS file connected successfully");
+});
+
+// load experience section
+loadExperience = (exp_data) => {
+
+    let exp = ''
+    var isLeft = "left"
+    var counter = 0
+
+    exp_data.sort((a,b) => {
+
+        if ( a.Serial_Number < b.Serial_Number )
+            return 1;
+        
+        if ( a.Serial_Number > b.Serial_Number )
+            return -1;
+        
+        return 0;
+
+    })
+    
+    exp_data.forEach(element => {
+
+        //console.log(Object.keys(element))
+        Object.keys(element).forEach(key => {
+            //console.log(key)
+            if (element[key] === '' || element[key] === "" || element[key].length === 0 ) {
+                //console.log(`Delete ${key}`)
+                delete element[key];
+            }
+        });
+
+        isExpanded.push(false)
+        exp += `
+            <div class="containerTimeline ${isLeft}">
+                <div class="content">
+                    <h1 style="font-size:2.5em">${element.Duration}</h1>
+                    <img alt="" src="${googleEmbedImage(element.Logo_Url)}" onerror="this.style.display='none'" class="work_exp_logo" />
+                    <div class="readmore" onclick="showhideexp(event,${counter})" >`
+
+                    if( "Organization" in element )
+                        exp += `<div><u><b>Organization</b></u>: ${element.Organization}</div>`
+                    if( "Role" in element )
+                        exp += `<div><u><b>Role</b></u>: ${element.Role}</div>`
+                    if( "Location" in element )
+                        exp += `<div><u><b>Location</b></u>: ${element.Location}</div>`
+                    if( "Department" in element )                       
+                        exp += `<div><u><b>Department</b></u>: ${element.Department}</div>`
+                    if( "Project" in element )
+                        exp += `<div><u><b>Project</b></u>: ${element.Project}</div>`
+                    if( "Client" in element )
+                        exp += `<div><u><b>Client</b></u>: ${element.Client}</div>`
+                    if( "Description" in element )
+                        exp += `<div><u><b>Description</b></u>: ${element.Description}</div>`
+                    if( "Responsibilities" in element ){
+                        exp += `<div><u><b>Responsibilities</b></u>:
+                            <ul>`
+
+        element.Responsibilities.forEach(resp => {
+            exp += `            <li>${resp}</li>`
+        })
+
+        exp += `            </ul>
+                        </div>`
+                    }
+                    if( "IDE/Compilers/Tools" in element ){
+                        exp += `<div><u><b>IDE/Compilers/Tools</b></u>:
+                            <ul>`
+
+        element["IDE/Compilers/Tools"].forEach(resp => {
+            exp += `            <li>${resp}</li>`
+        })
+
+        exp += `            </ul>
+                        </div>`
+                    }
+                        exp += `<div class="readmore-link" style="color: var(--link-color)"></div>
+                    </div>
+                </div>
+            </div>`
+
+        isLeft = "right"
+        counter++
+    })
+
+    document.getElementById('work-exp').innerHTML = exp
+}
+
+showhideexp = (e, s_no) => {
+
+    var isOpen = isExpanded[s_no]
+
+    // record if our text is expanded
+    //console.log(s_no)
+    //console.log(isExpanded)
+
+    //close all open paragraphs
+    $(".readmore.expand").removeClass("expand");
+    $(".readmore-link.expand").removeClass("expand");
+    isExpanded = new Array(isExpanded.length).fill(false)
+        //console.log(isExpanded)
+
+    if (isOpen)
+        return;
+
+    // if target wasn't expand, then expand it
+    if (!isExpanded[s_no]) {
+        $(e.target).parent(".readmore").addClass("expand");
+        $(e.target).addClass("expand");
+    }
+
+    isExpanded[s_no] = !isExpanded[s_no]
+
+    //console.log(isExpanded)
+}
---
+++ bin/src/main/resources/static/JS/SkillsNProjects.js
@@ -0,0 +1,247 @@
+let stat_type = ["Completed ", "Accuracy "]
+let cat_type = ["all", "easy", "medium", "hard"]
+var data_leetcode = {}
+var data_github = {}
+
+let SubmissionInformation = []
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ To be fetched from Database ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+var leetcodeData = {
+    "data": {
+        "allQuestionsCount": [{
+                "difficulty": "All",
+                "count": 2314
+            },
+            {
+                "difficulty": "Easy",
+                "count": 579
+            },
+            {
+                "difficulty": "Medium",
+                "count": 1230
+            },
+            {
+                "difficulty": "Hard",
+                "count": 505
+            }
+        ],
+        "matchedUser": {
+            "submitStats": {
+                "acSubmissionNum": [{
+                        "difficulty": "All",
+                        "count": 537,
+                        "submissions": 704
+                    },
+                    {
+                        "difficulty": "Easy",
+                        "count": 211,
+                        "submissions": 287
+                    },
+                    {
+                        "difficulty": "Medium",
+                        "count": 289,
+                        "submissions": 375
+                    },
+                    {
+                        "difficulty": "Hard",
+                        "count": 37,
+                        "submissions": 42
+                    }
+                ],
+                "totalSubmissionNum": [{
+                        "difficulty": "All",
+                        "count": 553,
+                        "submissions": 1041
+                    },
+                    {
+                        "difficulty": "Easy",
+                        "count": 211,
+                        "submissions": 358
+                    },
+                    {
+                        "difficulty": "Medium",
+                        "count": 299,
+                        "submissions": 612
+                    },
+                    {
+                        "difficulty": "Hard",
+                        "count": 43,
+                        "submissions": 71
+                    }
+                ]
+            }
+        }
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ To be fetched from Database ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// functions to be performed when webpage loads
+$(document).ready(function() {
+
+    thoughtboxdisplayed = false
+
+    // function to receieve LeetCode object datatype from controller
+
+    setTimeout( () => {
+        $.ajax({
+            type: 'GET',
+            url: formAjaxUrl("receiveLeetCodeData"),
+            contentType: "application/json",
+            headers: {
+                'session-uid':makeid()
+            },
+            dataType: 'json',
+            success: (data_leetcode) => {
+                if (data_leetcode.status >= 200 && data_leetcode.status < 400) {
+                    console.log("AJAX RESPONSE >> Success Leetcode")
+                    document.getElementById("leetcode_error").style.display = 'none'
+                    document.getElementById("github_container").style.display = 'block'
+                    loadLeetCodeView(data_leetcode.body)
+                } else {
+                    console.log("AJAX RESPONSE >> Error Leetcode")
+                    document.getElementById("coding_container").style.display = 'none'
+                    document.getElementById("leetcode_error").style.display = 'flex'
+                        //loadLeetCodeView(leetcodeData)
+                }
+            },
+            error: (err) => {
+                console.log("AJAX RESPONSE >> Error Leetcode")
+                document.getElementById("coding_container").style.display = 'none'
+                document.getElementById("leetcode_error").style.display = 'flex'
+                    //loadLeetCodeView(leetcodeData)
+            }
+        })
+    },1000);
+
+    //loadLeetCodeView(leetcodeData)
+    // Logic for handling leetcode question category stats
+    $("#c1").on(
+        'mouseenter click',
+        function() {
+            loadParticularCategoryData(1)
+        }
+    )
+    $("#c2").on(
+        'mouseenter click',
+        function() {
+            loadParticularCategoryData(2)
+        }
+    )
+    $("#c3").on(
+        'mouseenter click',
+        function() {
+            loadParticularCategoryData(3)
+        }
+    )
+    $(".category_class").on(
+        'mouseleave',
+        function() {
+            loadParticularCategoryData(0)
+        }
+    )
+
+    // function to receieve Github object datatype from controller
+    setTimeout( () => {
+        $.ajax({
+            type: 'GET',
+            url: formAjaxUrl("receiveGithubData"),
+            contentType: "application/json",
+            headers: {
+                'session-uid':makeid()
+            },
+            dataType: 'json',
+            success: (data_github) => {
+                if (data_github.status >= 200 && data_github.status < 400) {
+                    console.log("AJAX RESPONSE >> Github Success")
+                    document.getElementById("github_error").style.display = 'none'
+                    document.getElementById("github_container").style.display = 'block'
+                    loadGithubView(data_github.body)
+                } else {
+                    console.log("AJAX RESPONSE >> Github Error")
+                    document.getElementById("github_container").style.display = 'none'
+                    document.getElementById("github_error").style.display = 'flex'
+                }
+            },
+            error: (err) => {
+                console.log("AJAX RESPONSE >> Github Error")
+                document.getElementById("github_container").style.display = 'none'
+                document.getElementById("github_error").style.display = 'flex'
+            }
+        })
+    },1000);
+
+    console.log("Skills and Projects JS file connected successfully");
+    window.addEventListener('resize', () => { loadSkills() })
+});
+
+// Load the Leetcode statistics section
+loadLeetCodeView = (leetcodedata) => {
+
+    // 0 - All, 1 - Easy, 2 - Medium, 3 - Hard
+    for (var i = 0; i < 4; i++)
+        SubmissionInformation[i] = {
+            // Total questions in ith category
+            Questions: leetcodedata.data.allQuestionsCount[i].count,
+            // Total accepted solutions in ith category
+            Accepted: leetcodedata.data.matchedUser.submitStats.acSubmissionNum[i].submissions,
+            // Total unique questions solved in ith category
+            Solved: leetcodedata.data.matchedUser.submitStats.acSubmissionNum[i].count,
+            // Total accuracy in ith category
+            Accuracy_percent: Math.round(leetcodedata.data.matchedUser.submitStats.acSubmissionNum[i].submissions / leetcodedata.data.matchedUser.submitStats.totalSubmissionNum[i].submissions * 100 * 100) / 100,
+            // Percent of questions solved in ith category
+            Completed_percent: Math.round(leetcodedata.data.matchedUser.submitStats.acSubmissionNum[i].count / leetcodedata.data.allQuestionsCount[i].count * 100 * 100) / 100,
+        }
+
+    loadParticularCategoryData(0)
+}
+
+// modify the stats for a particular category
+loadParticularCategoryData = (category) => {
+
+    progressBar(SubmissionInformation[category].Completed_percent, 0);
+    progressBar(SubmissionInformation[category].Accuracy_percent, 1);
+
+    document.getElementById('progress_bar_container').innerHTML =
+        ` 
+        <div style="padding:0 10px 0 10px;"> Total questions: ${SubmissionInformation[category].Questions} </div> 
+        <div style = "padding:0 10px 0 10px;"> Solved questions: ${SubmissionInformation[category].Solved} </div> 
+        <div style = "padding:0 10px 0 10px;"> Accepted Solutions: ${SubmissionInformation[category].Accepted} </div>`
+
+    document.getElementsByClassName("progress-circle-prog")[0].style.stroke = `var(--color-${cat_type[category]})`;
+    document.getElementsByClassName("progress-circle-prog")[1].style.stroke = `var(--color-${cat_type[category]})`;
+    document.getElementsByClassName("progress-circle-back")[0].style.stroke = `var(--color-${cat_type[category]}-back)`;
+    document.getElementsByClassName("progress-circle-back")[1].style.stroke = `var(--color-${cat_type[category]}-back)`;
+}
+
+// function to dynamically modify the displayed percent
+progressBar = (progressVal, class_number) => {
+    var strokeVal = (4.64 * 100) / 100;
+    var x = document.getElementsByClassName('progress-circle-prog')[class_number];
+    x.style.strokeDasharray = progressVal * (strokeVal) + ' 999';
+    document.getElementsByClassName('progress-text')[class_number].innerHTML = stat_type[class_number] + progressVal + '%';
+    $('.progress-text').data('progress', progressVal);
+
+
+}
+
+// Load the Github statistics section
+loadGithubView = (Githubdata = GithubJSON) => {
+
+    let tabledata = `<table id="customers">
+                <tr>
+                  <th>Project Name</th>
+                  <th>Short Description</th>
+                </tr>`
+
+    Githubdata.data.user.pinnedItems.nodes.forEach(project => {
+        tabledata += `<tr>
+                        <td><a href="${project.url}" target="_blank">${project.name}</a></td>
+                        <td>${project.description}</td>
+                        </tr>`
+    })
+    tabledata += `</table>`
+
+    document.getElementById('github_projects').innerHTML = tabledata
+}
